43c377d68197ee05380d4e2cec9e7923d74db42d
Neville <live.neville@live.com>
2024-02-21 02:13:36 -0700

    add model

---
+++ src/AppCenterDownloader.MobileApp/ViewModels/AccountEditorViewModel.cs
@@ -0,0 +1,25 @@
+﻿using AppCenterDownloader.MobileApp.Models;
+using AppCenterDownloader.MobileApp.Services;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCenterDownloader.MobileApp.ViewModels
+{
+    public partial class AccountEditorViewModel(CentralService service)
+    {
+        private readonly CentralService _service = service;
+
+        public AddAccountModel Model { get; set; }
+
+        [RelayCommand]
+        async Task SaveAsync()
+        {
+            await _service.AddAsync(Model);
+        }
+
+    }
+}
