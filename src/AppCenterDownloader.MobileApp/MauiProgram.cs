508d3776d1668dc0cd3e21a1e554eef5829698ea
Neville <live.neville@live.com>
2024-02-21 02:11:02 -0700

    tool kit

--- src/AppCenterDownloader.MobileApp/MauiProgram.cs
+++ src/AppCenterDownloader.MobileApp/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using CommunityToolkit.Maui.Markup;
+using Microsoft.Extensions.Logging;
 
 namespace AppCenterDownloader.MobileApp
 {
@@ -9,6 +11,8 @@ namespace AppCenterDownloader.MobileApp
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
+                .UseMauiCommunityToolkitMarkup()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
