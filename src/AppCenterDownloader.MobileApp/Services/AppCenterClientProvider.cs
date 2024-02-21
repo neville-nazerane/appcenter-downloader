ba53bc15fddeb0db187c91fab418bb004a6a8222
Neville <live.neville@live.com>
2024-02-20 21:13:30 -0700

    service providers

---
+++ src/AppCenterDownloader.MobileApp/Services/AppCenterClientProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCenterDownloader.MobileApp.Services
+{
+    public class AppCenterClientProvider(HttpClient httpClient)
+    {
+        private readonly HttpClient _httpClient = httpClient;
+
+        public AppCenterClient GetClient(string key) => new AppCenterClient(_httpClient, key);
+
+    }
+}
--- src/AppCenterDownloader.MobileApp/Services/LocalRepository.cs
+++ src/AppCenterDownloader.MobileApp/Services/LocalRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AppCenterDownloader.MobileApp.Models.LocalDb;
+using LiteDB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +8,11 @@ using System.Threading.Tasks;
 
 namespace AppCenterDownloader.MobileApp.Services
 {
-    public class LocalRepository
+    public class LocalRepository(ILiteDatabase database)
     {
+        private readonly ILiteDatabase database = database;
 
+        public ILiteCollection<DbAccount> Accounts => database.GetCollection<DbAccount>();
 
 
     }
